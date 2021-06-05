19bd042089b19043c428fb4e2c1b0b357136c216
Adrian Marrero <adrianmarrero18@gmail.com>
2021-06-05 15:52:05 -0400

    class array, pop method and delete method created

--- javascript/array.js
+++ javascript/array.js
@@ -1,17 +1,50 @@
 class ArrayClass {
-    constructor(){
+    constructor()
+    {
         this.length = 0
         this.data = {}
     }
 
-    get(index){
+    get(index)
+    {
         return this.data[index]
     }
 
-    push(item){
+    push(item)
+    {
         this.data[this.length] = item
         this.length++
+
         return this.data
+    }
+
+    pop()
+    {
+        let last_value = this.data[this.length - 1]
+
+        delete this.data[this.length - 1]
+        
+        this.length--
+
+        return last_value
+    }
+
+    deleteIndex(index)
+    {
+        let element = this.data[index]
+
+        this.shiftIndex(index)
+
+        return element
+    }
+
+    shiftIndex(index)
+    {
+        for(let i = index; i < this.length-1; i++){
+            this.data[i] = this.data[i+1]
+        }
+        delete this.data[this.length-1]
+        this.length--
     }
 }
 
