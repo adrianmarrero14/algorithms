736f640c76aebd30f15d6f4a39ae00011f4f3967
Adrian Marrero <adrianmarrero18@gmail.com>
2021-06-05 15:20:51 -0400

    basic array

---
+++ javascript/array.js
@@ -0,0 +1,7 @@
+const nombres = ['Adrian', 'Juan', 'Andrea']
+
+console.log(nombres) // >> ['Adrian', 'Juan', 'Andrea']
+
+nombres.push('Mariana') // Add 'Mariana to the end'
+
+console.log(nombres) // >> ['Adrian', 'Juan', 'Andrea', 'Mariana']
