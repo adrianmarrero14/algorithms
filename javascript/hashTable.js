cf77c96a61b1787c785a8a14c729b283919941b7
Adrian Marrero <adrianmarrero18@gmail.com>
2021-06-06 12:38:01 -0400

    hashTable completed

--- javascript/hashTable.js
+++ javascript/hashTable.js
@@ -3,15 +3,23 @@ class HashTable {
     {
         this.data = new Array(size)
     }
-    haxhMethod(key)
+    hashMethod(key) 
     {
         let hash = 0
-
         for(let i = 0; i < key.length; i++){
             hash = (hash + key.charCodeAt(i) * i) % this.data.length
         }
-
         return hash
+    }
+
+    set(key, value)
+    {
+        const address = this.hashMethod(key)
+        if(!this.data[address]){
+            this.data[address] = []
+        }
+        this.data[address].push([key, value])
+        return this.data
     }
 }
 
