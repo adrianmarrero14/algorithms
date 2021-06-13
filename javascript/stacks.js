a24559de411f3bf4e2900a5146b9c06bd9f743d2
Adrian Marrero <adrianmarrero18@gmail.com>
2021-06-13 17:19:07 -0400

    Stack created with peek and push method

---
+++ javascript/stacks.js
@@ -0,0 +1,33 @@
+class Node {
+    constructor(value){
+        this.value = value
+        this.next = null
+    }
+}
+class Stack {
+    constructor(){
+        this.top = null
+        this.bottom = null
+        this.length = 0
+    }
+
+    peek(){
+        return this.top
+    }
+
+    push(value){
+        const newNode = new Node(value)
+        if(this.length === 0){
+            this.top = newNode
+            this.bottom = newNode
+        } else {
+            const holdingPointer = this.top
+            this.top = newNode
+            this.top.next = holdingPointer
+        }
+        this.length++
+        return this
+    }
+}
+
+const myStack = new Stack
