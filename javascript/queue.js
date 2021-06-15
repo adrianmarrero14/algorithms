a778c2643eb5f5c87f03fa23ebd592b696e1b4cf
Adrian Marrero <adrianmarrero18@gmail.com>
2021-06-15 19:52:48 -0400

    Queue base code

---
+++ javascript/queue.js
@@ -0,0 +1,25 @@
+class Node 
+{
+    constructor(value)
+    {
+        this.value = value
+        this.next = null
+    }
+}
+
+class Queue 
+{
+    constructor()
+    {
+        this.first = null
+        this.last = null
+        this.length = 0
+    }
+
+    peek()
+    {
+        return this.first
+    }
+}
+
+const myQueue = new Queue()
