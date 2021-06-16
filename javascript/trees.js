9c061373509222cca20bd532d29688c78c554ed7
Adrian Marrero <adrianmarrero18@gmail.com>
2021-06-15 20:59:20 -0400

    binarysearchtree class and insert method created

---
+++ javascript/trees.js
@@ -0,0 +1,44 @@
+class Node 
+{
+    constructor(value)
+    {
+        this.left = null
+        this.right = null
+        this.value = value
+    }
+}
+
+class BinarySearchTree
+{
+    constructor()
+    {
+        this.root = null
+    }
+
+    insert(value)
+    {
+        const newNode = new Node(value)
+        if(!this.root) {
+            this.root = newNode
+        } else {
+            let currentNode = this.root
+            while(true) {
+                if(value < currentNode.value){
+                    if(!currentNode.left){
+                        currentNode.left = newNode
+                        return this
+                    }
+                    currentNode = currentNode.left
+                } else {
+                    if(!currentNode.rigth){
+                        currentNode.right = newNode
+                        return this
+                    }
+                    currentNode = currentNode.right
+                }
+            }
+        }
+    }
+}
+
+const tree = new BinarySearchTree()
