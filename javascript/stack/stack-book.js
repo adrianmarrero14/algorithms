1db16562b6b3f83180b4dd577091e23214a516cd
Adrian Marrero <adrianmarrero18@gmail.com>
2021-06-18 14:54:37 -0400

    Stack class created with all methods

--- javascript/stack/stack-book.js
+++ javascript/stack/stack-book.js
@@ -279,4 +279,53 @@ class Stack {
     constructor(){
         this.linkedList = new LinkedList()
     }
+
+    /**
+     * @return {boolean}
+     */
+    isEmpty(){
+        return !this.linkedList.head
+    }
+
+    /**
+     * @return {*}
+     */
+    peek(){
+        if(this.isEmpty()){
+            return null
+        }
+        return this.linkedList.head.value
+    }
+
+    /**
+     * @param {*} value
+     */
+    push(value){
+        this.linkedList.prepend(value)
+    }
+
+    /**
+     * @return {*}
+     */
+    pop(){
+        const removedHead = this.linkedList.deleteHead()
+        return removedHead ? removedHead.value : null
+    }
+
+    /**
+     * @return {*[]}
+     */
+    toArray(){
+        return this.linkedList
+            .toArray()
+            .map((LinkedListNode) => LinkedListNode.value)
+    }
+
+    /**
+     * @param {function} [callback]
+     * @return {string}
+     */
+    toString(callback){
+        return this.linkedList.toString(callback)
+    }
 }
