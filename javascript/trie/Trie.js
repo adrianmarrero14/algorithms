6d9c8d471c8e3f58be5afebd197fd00b56776f2e
adrianmarrero <adrianmarrero18@gmail.com>
2021-07-20 11:44:37 -0400

    getLastCharacterNode method created

--- javascript/trie/Trie.js
+++ javascript/trie/Trie.js
@@ -516,9 +516,25 @@ class Trie {
      * @return {boolean}
      */
     doesWordExist(word){
-        const lastCharacter = this.getLastCharacterNode(word);
-        return !!lastCharacter && lastCharacter.isCompleteWord;
+        const lastCharacter = this.getLastCharacterNode(word)
+        return !!lastCharacter && lastCharacter.isCompleteWord
     }
 
-    /** */
+    /** 
+     * @param {string} word
+     * @return {TrieNode}
+    */
+   getLastCharacterNode(word){
+       const characters = Array.from(word)
+       let currentNode = this.head
+
+       for(let charIndex = 0; charIndex < characters.length; charIndex += 1){
+           if(!currentNode.hasChild(characters[charIndex])){
+                return null
+           }
+           currentNode = currentNode.getChild(characters[charIndex])
+       }
+
+       return currentNode
+   }
 }
